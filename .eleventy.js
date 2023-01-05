a91dbb26003c2ee6c7135bea55a145183c886197
inescodes <ineskahloula@hotmail.com>
2023-01-05 21:14:02 +0100

    add fonts

--- .eleventy.js
+++ .eleventy.js
@@ -1,8 +1,13 @@
 module.exports = (eleventyConfig) => {
+	// styles
 	eleventyConfig.addWatchTarget('src/assets/scss');
 	eleventyConfig.addPassthroughCopy({ 'src/assets/css': 'css/' });
 
+	// images
 	eleventyConfig.addPassthroughCopy({ 'src/assets/images': '/' });
+
+	// fonts
+	eleventyConfig.addPassthroughCopy({ 'src/assets/fonts': '/' });
 
 	return {
 		markdownTemplateEngine: 'njk',
