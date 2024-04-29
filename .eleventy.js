eaf9d92d935758f3d70a36994b82d51fa83352c3
Ines <ineskahloula@hotmail.com>
2024-04-29 20:40:09 +0100

    ❌ remove unnecessary js files from dist/

--- .eleventy.js
+++ .eleventy.js
@@ -1,11 +1,5 @@
 module.exports = (eleventyConfig) => {
-	// styles
 	eleventyConfig.addWatchTarget('src/assets/scss');
-
-	// js
-	eleventyConfig.addPassthroughCopy({ 'src/assets/js': '/' });
-
-	// icons
 	eleventyConfig.addPassthroughCopy({ 'src/assets/icons': '/' });
 
 	return {
