acd1e9ff0324f433a6a46562e62aec7c08e06dcb
Ines <ineskahloula@hotmail.com>
2024-05-03 20:01:28 +0100

    🔗 update bookmark cards to behave as clickable links

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,4 +1,5 @@
 import './modules/menu.js';
+import './modules/bookmarks.js';
 
 import('./modules/dragAndDrop.createDraggables.js').then((createDraggables) => {
 	createDraggables.default();
---
+++ src/assets/js/modules/bookmarks.js
@@ -0,0 +1,23 @@
+class Bookmarks {
+	constructor() {
+		this.list = document.querySelector('.bookmarks');
+		this.items = Array.from(this.list.querySelectorAll('.bookmark'));
+		this.init();
+	}
+
+	init() {
+		this.items.forEach((item) => {
+			item.addEventListener('click', this.handleClick);
+			item.setAttribute('data-clickable', '');
+		});
+	}
+
+	handleClick(e) {
+		const link = e.currentTarget.querySelector('a[href]');
+		link.click();
+	}
+}
+
+if (document.querySelector('.bookmarks')) {
+	new Bookmarks();
+}
