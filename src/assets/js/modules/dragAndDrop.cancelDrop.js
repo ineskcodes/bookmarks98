fee63ce5d19c157ed4632e4aefe6911ffeeca9a2
Ines <40505449+ineskcodes@users.noreply.github.com>
2024-05-05 23:03:02 +0100

    fix: icons drag n drop bug

--- src/assets/js/modules/dragAndDrop.cancelDrop.js
+++ src/assets/js/modules/dragAndDrop.cancelDrop.js
@@ -1,9 +1,7 @@
 import gsap from 'gsap';
-import { getLastPositionFromLocalStorage } from './utils';
 
 function cancelDrop(draggable) {
 	const { target } = draggable;
-	const lastPosition = getLastPositionFromLocalStorage(draggable);
 
 	gsap.to(target, {
 		duration: 0,
--- src/assets/js/modules/dragAndDrop.drop.js
+++ src/assets/js/modules/dragAndDrop.drop.js
@@ -2,26 +2,18 @@ import gsap from 'gsap';
 import {
 	getDraggableType,
 	getDraggableItemName,
-	getLastPositionFromLocalStorage,
 	populateStorage,
 } from './utils';
 
 function drop(draggable) {
 	let { x, y, target } = draggable;
-	const lastPosition = getLastPositionFromLocalStorage(draggable);
 	const draggableType = getDraggableType(draggable);
 	const item = getDraggableItemName(draggable);
-
-	if (lastPosition) {
-		x += lastPosition.x;
-		y += lastPosition.y;
-	}
 
 	populateStorage(item, { x, y });
 
 	if (draggableType === 'icon') {
-		gsap.to(target, { duration: 0, x: 0, y: 0 });
-		gsap.to(target.parentElement, { duration: 0, x, y });
+		gsap.to(target.previousElementSibling, { duration: 0, x, y });
 	}
 }
 
