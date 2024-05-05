dde50bffc91ab8841d9dc30b932085043029be0b
Ines <40505449+ineskcodes@users.noreply.github.com>
2024-05-05 17:43:25 +0100

    🖼️ add wallpaper image files

--- .eleventy.js
+++ .eleventy.js
@@ -2,6 +2,7 @@ module.exports = (eleventyConfig) => {
 	eleventyConfig.addWatchTarget('src/assets/scss');
 	eleventyConfig.addPassthroughCopy({ 'src/assets/icons': '/' });
 	eleventyConfig.addPassthroughCopy({ 'src/assets/misc': '/' });
+	eleventyConfig.addPassthroughCopy({ 'src/assets/wallpapers': '/' });
 
 	return {
 		markdownTemplateEngine: 'njk',
