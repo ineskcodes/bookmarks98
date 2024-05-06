3a44fbc2c441c7866b819d6702f382a4d20cc640
Ines <40505449+ineskcodes@users.noreply.github.com>
2024-05-06 01:08:24 +0100

    save and set icons' positions on page load from localStorage

--- src/assets/js/modules/dragAndDrop.cancelDrop.js
+++ src/assets/js/modules/dragAndDrop.cancelDrop.js
@@ -1,12 +1,14 @@
 import gsap from 'gsap';
+import { getLastPositionFromLocalStorage } from './utils';
 
 function cancelDrop(draggable) {
 	const { target } = draggable;
+	const lastPosition = getLastPositionFromLocalStorage(draggable);
 
 	gsap.to(target, {
 		duration: 0,
-		x: 0,
-		y: 0,
+		x: lastPosition ? lastPosition.x : 0,
+		y: lastPosition ? lastPosition.y : 0,
 	});
 
 	target.style.zIndex = draggable.vars.zIndex.onDragEnd;
