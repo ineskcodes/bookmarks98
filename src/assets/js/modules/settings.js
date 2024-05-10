a6db67a396edbb0f933fa84c066381716126d449
Ines <ineskahloula@hotmail.com>
2024-05-10 21:28:11 +0100

    🖼️ add functionality to add custom wallpaper from user input

--- src/assets/js/modules/settings.js
+++ src/assets/js/modules/settings.js
@@ -1,4 +1,5 @@
 import { root, main, header, footer } from './elements';
+import { wallpapers } from './wallpapers';
 
 class SettingsPopup {
 	constructor() {
@@ -8,15 +9,26 @@ class SettingsPopup {
 		this.closeButton = this.container.querySelector('#settings-close-btn');
 		this.cancelButton = this.container.querySelector('#settings-cancel-btn');
 		this.applyButton = this.container.querySelector('#settings-apply-btn');
+		this.browseInput = this.container.querySelector('#browse-input');
+		this.switcher = this.container.querySelector(
+			'.switcher__wallpapers:not([data-placeholder])'
+		);
 		this.form = this.container.querySelector('form');
 		this.previewBox = this.popup.querySelector('#wallpaper-preview');
-		this.wallpaperInputs = Array.from(
-			this.popup.querySelectorAll('input[name="wallpaper"]')
-		);
+
 		this.displaySelect = this.popup.querySelector('#display-select');
 		this.closers = [this.closeButton, this.cancelButton];
 		this.inerts = [main, header, footer, this.openButton];
 		this.init();
+		this.wallpapers = localStorage.getItem('wallpapers')
+			? JSON.parse(localStorage.getItem('wallpapers'))
+			: wallpapers.map((item) => {
+					return {
+						name: item.replaceAll(' ', '-').toLowerCase(),
+						url: `../${item.replaceAll(' ', '-').toLowerCase()}.png`,
+						enabled: false,
+					};
+				});
 	}
 
 	init() {
@@ -29,18 +41,13 @@ class SettingsPopup {
 		this.closers.forEach((closerEl) =>
 			closerEl.addEventListener('click', () => this.closePopup())
 		);
-		this.wallpaperInputs.forEach((input) =>
-			input.addEventListener('input', (e) => {
-				this.setWallpaper(e, this.previewBox);
-				this.scrollToInput(e);
-			})
-		);
 
 		this.displaySelect.addEventListener('input', (e) =>
 			this.setDisplayMode(e, this.previewBox)
 		);
 		this.applyButton.addEventListener('click', (e) => this.applySettings(e));
 		this.form.addEventListener('submit', (e) => this.handleSubmit(e));
+		this.browseInput.addEventListener('input', (e) => this.handleUpload(e));
 	}
 
 	openPopup() {
@@ -48,6 +55,14 @@ class SettingsPopup {
 		this.openButton.setAttribute('aria-expanded', 'true');
 		this.popup.focus();
 		this.inerts.forEach((el) => el.setAttribute('inert', ''));
+
+		this.wallpaperInputs = Array.from(
+			this.switcher.querySelectorAll('input[name="wallpaper"]')
+		);
+
+		this.wallpaperInputs.forEach((input) =>
+			input.addEventListener('input', (e) => this.handleWallpaperInput(e))
+		);
 	}
 
 	closePopup() {
@@ -57,32 +72,37 @@ class SettingsPopup {
 		this.inerts.forEach((el) => el.removeAttribute('inert'));
 	}
 
-	scrollToInput(e) {
-		e.currentTarget.parentElement.scrollIntoView({ block: 'center' });
+	scrollToInput(input) {
+		input.parentElement.scrollIntoView({ block: 'center' });
+		input.focus();
 	}
 
-	setWallpaper(e, el, value) {
-		const wallpaper = e ? e.currentTarget.value : value;
-
-		if (wallpaper !== 'None') {
-			el.style.setProperty('--wallpaper', `url("../${wallpaper}.png")`);
-		}
+	setWallpaper(el, url) {
+		el.style.setProperty('--wallpaper', `url("${url}")`);
 	}
 
 	setDisplayMode(e, el, value) {
 		const displayMode = e ? e.currentTarget.value : value;
+		let previewSize =
+			this.widthPercentage && this.heightPercentage
+				? `${this.widthPercentage}% ${this.heightPercentage}%`
+				: localStorage.getItem('previewSize');
+
+		if (el === this.previewBox && displayMode !== 'stretch') {
+			localStorage.setItem('previewSize', previewSize);
+		}
 
 		switch (displayMode) {
 			case 'center':
 				el.style.setProperty(`--size`, 'auto');
-				el.style.setProperty('--preview-size', 'auto');
+				el.style.setProperty('--preview-size', previewSize);
 				el.style.setProperty('--repeat', 'no-repeat');
 				el.style.setProperty('--position', 'center');
 				break;
 
 			case 'tile':
 				el.style.setProperty(`--size`, 'auto');
-				el.style.setProperty('--preview-size', '22.5% auto');
+				el.style.setProperty('--preview-size', previewSize);
 				el.style.setProperty('--repeat', 'repeat');
 				el.style.setProperty('--position', 'auto');
 				break;
@@ -98,12 +118,27 @@ class SettingsPopup {
 
 	applySettings() {
 		const displayMode = this.displaySelect.value;
-		const wallpaper = this.wallpaperInputs.find((input) => input.checked).value;
+		const input = this.wallpaperInputs.find((input) => input.checked);
 
-		this.setWallpaper(null, root, wallpaper);
+		this.wallpapers.forEach((item) => (item.enabled = false));
+		const wallpaper = this.wallpapers.find((item) => {
+			return item.name === input.value;
+		});
+		wallpaper.enabled = true;
+		this.setWallpaper(
+			root,
+			wallpaper.custom ? wallpaper.base64 : wallpaper.url
+		);
 		this.setDisplayMode(null, root, displayMode);
 
-		localStorage.setItem('wallpaper', wallpaper);
+		const switcherInnerHTML = {
+			innerHTML: this.container.querySelector(
+				'.switcher__wallpapers:not([data-placeholder])'
+			).innerHTML,
+		};
+
+		localStorage.setItem('switcher', JSON.stringify(switcherInnerHTML));
+		localStorage.setItem('wallpapers', JSON.stringify(this.wallpapers));
 		localStorage.setItem('display', displayMode);
 	}
 
@@ -112,6 +147,114 @@ class SettingsPopup {
 
 		this.applySettings();
 		this.closePopup();
+	}
+
+	handleWallpaperInput(e) {
+		const input = e.currentTarget;
+		const wallpaper = this.wallpapers.find((item) => {
+			return item.name === input.value;
+		});
+		const url = wallpaper.custom ? wallpaper.base64 : wallpaper.url;
+		this.wallpaperInputs.forEach((input) => {
+			input.removeAttribute('checked');
+			input.classList.remove('applied');
+		});
+
+		input.setAttribute('checked', 'checked');
+		input.classList.add('applied');
+
+		this.scrollToInput(e.currentTarget);
+
+		const img = document.createElement('img');
+		img.src = wallpaper.custom
+			? wallpaper.base64
+			: `/bookmarks98/${wallpaper.name}.png`;
+		const displayMode = this.displaySelect.value;
+
+		img.onload = () => {
+			const { naturalWidth, naturalHeight } = img;
+			const rootWidth = root.offsetWidth;
+			const rootHeight = root.offsetHeight;
+
+			this.widthPercentage = Math.round((naturalWidth * 100) / rootWidth);
+			this.heightPercentage = Math.round((naturalHeight * 100) / rootHeight);
+
+			this.setWallpaper(this.previewBox, url);
+			this.setDisplayMode(null, this.previewBox, displayMode);
+		};
+	}
+
+	handleUpload(e) {
+		const file = e.currentTarget.files[0];
+		const fileName = file.name.replace(/.(jpg|png|jpeg)$/, '');
+		const id = fileName.replaceAll(' ', '-').toLowerCase();
+		const imgSrc = this.switcher.querySelector('img').src;
+		const url = window.URL.createObjectURL(file);
+		const size = file.size;
+		const isDuplicate = !!this.wallpapers.find((item) => item.name === id);
+
+		if (size > 1_048_576) {
+			alert('File Size too big!');
+			return;
+		}
+
+		if (isDuplicate) {
+			alert(`${fileName} already exists!`);
+			return;
+		}
+
+		const label = document.createElement('label');
+		label.setAttribute('for', id);
+		label.classList.add('wallpaper');
+
+		const input = document.createElement('input');
+		input.setAttribute('type', 'radio');
+		input.setAttribute('name', 'wallpaper');
+		input.id = id;
+		input.value = id;
+
+		input.setAttribute('checked', 'checked');
+		input.classList.add('applied');
+
+		const img = document.createElement('img');
+		img.width = 16;
+		img.height = 16;
+		img.src = imgSrc;
+		img.alt = '';
+
+		const span = document.createElement('span');
+		span.innerText = fileName;
+
+		this.wallpaperInputs.push(input);
+
+		label.append(input, img, span);
+		this.switcher.append(label);
+
+		const image = document.createElement('img');
+		image.src = url;
+
+		image.onload = (e) => {
+			const image = e.currentTarget;
+			const canvas = document.createElement('canvas');
+			const ctx = canvas.getContext('2d');
+			canvas.width = image.width;
+			canvas.height = image.height;
+			ctx.drawImage(image, 0, 0, image.width, image.height);
+			const imageUrl = canvas.toDataURL('image/png');
+			this.addWallpaper(id, url, imageUrl);
+			input.addEventListener('input', (e) => this.handleWallpaperInput(e));
+			input.dispatchEvent(new Event('input'));
+		};
+	}
+
+	addWallpaper(wallpaperName, wallpaperUrl = null, imageUrl = null) {
+		this.wallpapers.push({
+			name: wallpaperName,
+			url: wallpaperUrl,
+			enabled: false,
+			custom: true,
+			base64: imageUrl,
+		});
 	}
 }
 
---
+++ src/assets/js/modules/wallpapers.js
@@ -0,0 +1,23 @@
+const wallpapers = [
+	'Black Thatch',
+	'Blue Rivets',
+	'Bubbles',
+	'Carved Stone',
+	'Circles',
+	'Clouds',
+	'Forest',
+	'Gold Weave',
+	'Houndstooth',
+	'Metal Links',
+	'Pinstripe',
+	'Red Blocks',
+	'Sandstone',
+	'Setup',
+	'Stitches',
+	'Straw Mat',
+	'Tiles',
+	'Triangles',
+	'Waves',
+];
+
+export { wallpapers };
