01a80d6953b38f1cab94e7a3b67c944bd28ff2a5
Ines <ineskahloula@hotmail.com>
2024-05-22 19:57:56 +0100

    🗣️ add plugin to update the contents of the head tag

--- src/assets/js/swup.js
+++ src/assets/js/swup.js
@@ -1,4 +1,7 @@
 import Swup from 'swup';
+import SwupHeadPlugin from '@swup/head-plugin';
+
 const swup = new Swup({
 	containers: ['#content', '#page-hook'],
+	plugins: [new SwupHeadPlugin()],
 });
