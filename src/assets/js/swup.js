4349f745b0022ee9e960bf059d766536b9292b9c
Ines <ineskahloula@hotmail.com>
2024-05-22 21:56:25 +0100

    🗣️ add plugin to reload scripts inside the head tag

--- src/assets/js/swup.js
+++ src/assets/js/swup.js
@@ -1,7 +1,8 @@
 import Swup from 'swup';
 import SwupHeadPlugin from '@swup/head-plugin';
+import SwupScriptsPlugin from '@swup/scripts-plugin';
 
 const swup = new Swup({
 	containers: ['#content', '#page-hook'],
-	plugins: [new SwupHeadPlugin()],
+	plugins: [new SwupHeadPlugin(), new SwupScriptsPlugin({ body: false })],
 });
