eea07810cd3bdfd9ddcc08537f3f2c2a924ad20a
Ines <ineskahloula@hotmail.com>
2024-06-27 15:20:03 +0100

    move critical js into its own file

--- .eleventy.js
+++ .eleventy.js
@@ -1,9 +1,24 @@
+const { minify } = require('terser');
+
 module.exports = (eleventyConfig) => {
 	eleventyConfig.addWatchTarget('src/assets/scss');
 	eleventyConfig.addPassthroughCopy({ 'src/assets/icons': '/' });
 	eleventyConfig.addPassthroughCopy({ 'src/assets/misc': '/' });
 	eleventyConfig.addPassthroughCopy({ 'src/assets/wallpapers': '/' });
 	eleventyConfig.addPassthroughCopy({ 'src/assets/themes': '/' });
+	eleventyConfig.addNunjucksAsyncFilter(
+		'jsmin',
+		async function (code, callback) {
+			try {
+				const minified = await minify(code);
+				callback(null, minified.code);
+			} catch (err) {
+				console.error('Terser error: ', err);
+				// Fail gracefully.
+				callback(null, code);
+			}
+		}
+	);
 
 	return {
 		markdownTemplateEngine: 'njk',
